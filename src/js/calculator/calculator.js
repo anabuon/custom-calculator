b77c07e26646b0876dc294269b914f30e5939c83
anabuon <anabuon@icloud.com>
2021-09-09 17:17:13 +0300

    refactor: add new metod

--- src/js/calculator/calculator.js
+++ src/js/calculator/calculator.js
@@ -1,6 +1,6 @@
 import { REST_OPERATIONS } from '../utils/constants.js'
-import { getDisplayNumber } from '../utils/getDisplayNumber'
-import { calcMathOperations } from '../utils/calcMathOperations'
+// import { getDisplayNumber } from '../utils/getDisplayNumber'
+// import { calcMathOperations } from '../utils/calcMathOperations'
 import { calcRestOperations } from '../utils/calcRestOperations'
 
 export default class Calculator {
@@ -12,9 +12,10 @@ export default class Calculator {
   }
 
   clear() {
-    this.currentOperand = '0'
-    this.previousOperand = ''
+    this.currentOperand = 0
     this.operation = undefined
+    this.previousOperand = ''
+    // this.previousOperandTextElement.innerText = ''
   }
 
   addToMemory() {
@@ -34,78 +35,70 @@ export default class Calculator {
     this.currentOperand = this.memory
   }
 
-  appendNumber(number) {
-    if (number === '.' && this.currentOperand.includes('.')) return
-
-    this.currentOperand = this.currentOperand.toString() + number.toString()
+  insert(string) {
+    if (this.currentOperand == '0') {
+      this.currentOperand = ''
+      this.currentOperand += string
+    } else {
+      this.currentOperand += string
+    }
   }
 
-  // appendNumberQQQ(number) {
-  //   if (number === '(' || number === ')') return
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand
+  }
 
-  //   this.currentOperand = this.currentOperand.toString() + number.toString()
-  // }
+  calc(calcType) {
+    const current = this.currentOperand
+
+    if (calcType === 'math') {
+      this.currentOperand = eval(current)
+      this.previousOperandTextElement.innerText = current
+    }
+
+    if (calcType === 'rest-operation') {
+      this._calcRest(eval(current))
+      this.previousOperandTextElement.innerText = current
+    }
+  }
 
   chooseOperation(operation) {
     if (this.currentOperand === '') return
 
     this.operation = operation
 
-    if (!this._checkIsRest(this.operation) && this.currentOperand !== '') {
-      this.calc('math')
-      this.previousOperand = this.currentOperand
-      this.currentOperand = ''
-    }
-
     if (this._checkIsRest(this.operation)) {
       this.calc('rest-operation')
     }
   }
 
-  calc(calcType) {
-    const current = parseFloat(this.currentOperand)
-
-    if (isNaN(current)) return
-
-    if (calcType === 'math') {
-      const prev = parseFloat(this.previousOperand)
-
-      if (isNaN(prev)) return
-
-      this._calcMath(prev, current)
-    }
-
-    if (calcType === 'rest-operation') {
-      this._calcRest(current)
-    }
-  }
-
-  updateDisplay() {
-    this.currentOperandTextElement.innerText = getDisplayNumber(
-      this.currentOperand,
-    )
-
-    if (!this._checkIsRest(this.operation)) {
-      this.previousOperandTextElement.innerText = `${getDisplayNumber(
-        this.previousOperand,
-      )} ${this.operation || ''}`
-      return
-    }
-
-    this.previousOperandTextElement.innerText = ''
+  _calcRest(current) {
+    this.currentOperand = calcRestOperations(this.operation, current)
   }
 
   _checkIsRest(operation) {
     return REST_OPERATIONS.includes(operation)
-  }
-
-  _calcMath(prev, current) {
-    this.currentOperand = calcMathOperations(this.operation, prev, current)
-    this.operation = undefined
-    this.previousOperand = ''
-  }
-
-  _calcRest(current) {
-    this.currentOperand = calcRestOperations(this.operation, current)
+    // return Object.keys(REST_OPERATIONS).includes(operation)
   }
 }
+
+//   _calcMath(prev, current) {
+//     this.currentOperand = calcMathOperations(this.operation, prev, current)
+//     this.operation = undefined
+//     this.previousOperand = ''
+//   }
+
+// appendNumber(number) {
+//   // console.log(number)
+//   if (number === '.' && this.currentOperand.includes('.')) return
+
+//   _checkBrackets(string) {
+//     this.opens = string.match(/\(/g) || []
+//     this.closes = string.match(/\)/g) || []
+//     return this.opens.length === this.closes.length
+//   }
+
+// changeRest(operation) {
+//   const key = Object.keys(REST_OPERATIONS).filter((el) => el === operation)
+//   return REST_OPERATIONS[key]
+// }
